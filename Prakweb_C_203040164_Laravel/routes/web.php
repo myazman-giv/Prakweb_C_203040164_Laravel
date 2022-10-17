f687ad8c2f79cec26edf0276f398396db0a32f3b
yazman-giv <myazman13@gmail.com>
2022-10-17 19:09:59 +0700

    Memodifikasi struktur folder & Membuat halaman : Home, About dan Blog.

--- Prakweb_C_203040164_Laravel/routes/web.php
+++ Prakweb_C_203040164_Laravel/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Mochamad Yazman Yazman",
+        "email" => "myazman9b@gmail.com",
+        "image" => "resize.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('post');
 });
