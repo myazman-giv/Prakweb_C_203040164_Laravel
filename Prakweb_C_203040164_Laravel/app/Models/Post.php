f77f62f4cb77f6b71ebb8a23dee0ba7ab50bd3c0
yazman-giv <myazman13@gmail.com>
2022-10-24 23:26:17 +0700

    Membuat Post model

--- Prakweb_C_203040164_Laravel/app/Models/Post.php
+++ Prakweb_C_203040164_Laravel/app/Models/Post.php
@@ -2,45 +2,13 @@
 
 namespace App\Models;
 
-class Post
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-            "title" => "Post Pertama",
-            "slug" => "Judul-post-pertama",
-            "author" => "Mochamad Yazman Yazid",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. A eum pariatur quidem! Ea, 
-            corrupti rerum. Quasi incidunt, nihil, mollitia eos soluta tempora, et esse ratione cupiditate 
-            ut ipsa cum tenetur modi harum numquam nesciunt doloribus optio distinctio cumque deserunt. 
-            Necessitatibus voluptates nulla reprehenderit nihil quo voluptatibus omnis tempore ad officiis 
-            est iste facere, ex alias, vero explicabo quia eius? Laboriosam aperiam laudantium vitae quae 
-            quasi fugit voluptas est explicabo cupiditate eum ducimus, recusandae facere placeat qui odit 
-            ab maxime deleniti excepturi tempora ratione autem impedit? Deleniti voluptate totam minima 
-            molestias, cupiditate cum id accusantium possimus? Quae possimus iste saepe nisi."
-        ],
-        [
-            "title" => "Post Iteer",
-            "slug" => "Judul-post-kedua",
-            "author" => "Walter Samuel",
-            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. A eum pariatur quidem! Ea, 
-            corrupti rerum. Quasi incidunt, nihil, mollitia eos soluta tempora, et esse ratione cupiditate 
-            ut ipsa cum tenetur modi harum numquam nesciunt doloribus optio distinctio cumque deserunt. 
-            Necessitatibus voluptates nulla reprehenderit nihil quo voluptatibus omnis tempore ad officiis 
-            est iste facere, ex alias, vero explicabo quia eius? Laboriosam aperiam laudantium vitae quae 
-            quasi fugit voluptas est explicabo cupiditate eum ducimus, recusandae facere placeat qui odit 
-            ab maxime deleniti excepturi tempora ratione autem impedit? kedua."
-        ]
-    ];
+    use HasFactory;
 
-
-    public static function all()
-    {
-        return collect(self::$blog_posts);
-    }
-
-    public static function find($slug)
-    {
-        $posts = static::all();
-        return $posts->firstWhere('slug', $slug);
-    }
+    //  protected $fillable = ['title', 'excerpt', 'body'];
+    protected $guarded = ['id'];
 }
--- Prakweb_C_203040164_Laravel/resources/views/post.blade.php
+++ Prakweb_C_203040164_Laravel/resources/views/post.blade.php
@@ -2,9 +2,9 @@
 
 @section('container')
 <article>
-  <h2>{{ $post["title"] }}</h2>
-  <h5>{{ $post["author"] }}</h5>
-  <p>{{ $post["body"] }}</p>
+  <h2>{{ $post->title }}</h2>
+  {{-- <h5>{{ $post["author"] }}</h5> --}}
+  {!! $post->body !!}
 </article>
     <a class="btn btn-primary" href="/posts">Back</a>
 @endsection
