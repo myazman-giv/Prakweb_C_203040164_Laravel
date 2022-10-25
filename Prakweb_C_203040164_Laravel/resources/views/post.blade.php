f11211f0b1ff4e250bf146df4ab69a0f15b27133
yazman-giv <myazman13@gmail.com>
2022-10-25 14:48:26 +0700

    Membuat Post Category

--- Prakweb_C_203040164_Laravel/resources/views/post.blade.php
+++ Prakweb_C_203040164_Laravel/resources/views/post.blade.php
@@ -3,6 +3,7 @@
 @section('container')
 <article>
   <h2>{{ $post->title }}</h2>
+  <p> By. Mochamad Yazman Yazid in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
   {{-- <h5>{{ $post["author"] }}</h5> --}}
   {!! $post->body !!}
 </article>
