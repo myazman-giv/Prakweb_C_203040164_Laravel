fe7c0e86987057ddcacd64f90f09adf45d2203fc
yazman-giv <myazman13@gmail.com>
2022-11-04 23:57:45 +0700

    Membuat registrasi untuk user

--- Prakweb_C_203040164_Laravel/app/Http/Controllers/RegisterController.php
+++ Prakweb_C_203040164_Laravel/app/Http/Controllers/RegisterController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -12,5 +14,25 @@ class RegisterController extends Controller
             'title' => 'Register',
             'active' => 'register'
         ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:30',
+            'username' => ['required', 'min:3', 'max:30', 'unique:users'],
+            'email' => 'required|email:dns|unique:users',
+            'password' => 'required|min:5|max:12'
+        ]);
+
+        //$validatedData['password'] = bcrypt($validatedData['password']);
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        User::create($validatedData);
+
+        // $request->session()->flash('success', 'Registrasi berhasil');
+
+
+        return redirect('/login')->with('success', 'Registrasi berhasil');
     }
 }
