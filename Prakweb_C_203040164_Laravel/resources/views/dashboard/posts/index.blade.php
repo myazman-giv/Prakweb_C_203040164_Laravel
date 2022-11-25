1785688f9219cf50f8786265bf67fd4ace29aed6
yazman-giv <myazman13@gmail.com>
2022-11-26 01:41:37 +0700

    Menambahkan fitur update dan delete

--- Prakweb_C_203040164_Laravel/resources/views/dashboard/posts/index.blade.php
+++ Prakweb_C_203040164_Laravel/resources/views/dashboard/posts/index.blade.php
@@ -6,7 +6,7 @@
 </div>
 
 @if(session()->has('success'))
-<div class="alert alert-success" role="alert">
+<div class="alert alert-success col-lg-8" role="alert">
   {{ session('success') }}
 </div> 
 @endif
@@ -28,9 +28,14 @@
         <td>{{ $loop->iteration }}</td>
         <td>{{ $post->title }}</td>
         <td>{{ $post->category->name }}</td>
-        <td><a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info"><span data-feather="eye" class="align-text-bottom"></span></a></td>
-        <td><a href="" class="badge bg-warning"><span data-feather="edit" class="align-text-bottom"></span></a></td>
-        <td><a href="" class="badge bg-danger"><span data-feather="x-circle" class="align-text-bottom"></span></a></td>
+        <td><a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info"><span data-feather="eye" class="align-text-bottom"></span></a>
+          <a href="/dashboard/posts/{{ $post->slug }}/edit" class="badge bg-warning"><span data-feather="edit" class="align-text-bottom"></span></a>
+          <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+            @method('delete')
+            @csrf
+            <button class="badge bg-danger border-0" onclick="return confirm('Are you sure?')"><span data-feather="x-circle" class="align-text-bottom"></span></button>
+          </form>
+        </td>
       </tr>
       @endforeach
     </tbody>
--- Prakweb_C_203040164_Laravel/resources/views/dashboard/posts/show.blade.php
+++ Prakweb_C_203040164_Laravel/resources/views/dashboard/posts/show.blade.php
@@ -6,8 +6,12 @@
     <div class="col-lg-8">
       <h2 class="mb-3">{{ $post->title }}</h2>
         <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left" class="align-text-bottom"></span> Back to my post</a>
-        <a href="" class="btn btn-warning"><span data-feather="edit" class="align-text-bottom"></span> Edit</a>
-        <a href="" class="btn btn-danger"><span data-feather="x-circle" class="align-text-bottom"></span> Delete</a>
+        <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning"><span data-feather="edit" class="align-text-bottom"></span> Edit</a>
+        <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+          @method('delete')
+          @csrf
+          <button class="btn btn-danger" onclick="return confirm('Are you sure?')"><span data-feather="x-circle" class="align-text-bottom"></span>Delete</button>
+        </form>
       {{-- <h5>{{ $post["author"] }}</h5> --}}
       <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid mt-3">
 
@@ -18,3 +22,4 @@
   </div>
 </div>
 @endsection
+
